decd3291cf380c787b757b12b8dcdfef518c481b
Raza403 <raza403@gmail.com>
2024-12-04 18:34:26 +0500

    Added delete option in enrollment method.

--- lib.php
+++ lib.php
@@ -33,10 +33,12 @@ defined('MOODLE_INTERNAL') || die();
 
 // This loads the language file for your plugin.
 require_once($CFG->dirroot . '/enrol/course_tokens/lang/en/enrol_course_tokens.php');
-class enrol_course_tokens_plugin extends enrol_plugin {
+class enrol_course_tokens_plugin extends enrol_plugin
+{
 
     // Override the get_name method to return 'course_tokens'
-    public function get_name() {
+    public function get_name()
+    {
         return 'course_tokens';
     }
 
@@ -48,7 +50,8 @@ class enrol_course_tokens_plugin extends enrol_plugin {
      * @param stdClass $instance Course enrol instance.
      * @return bool True means user with 'enrol/course_tokens:enrol' may enrol others freely, false means nobody may add more enrolments manually.
      */
-    public function allow_enrol($instance) {
+    public function allow_enrol($instance)
+    {
         return true;
     }
 
@@ -60,7 +63,8 @@ class enrol_course_tokens_plugin extends enrol_plugin {
      * @param stdClass $instance Course enrol instance.
      * @return bool True means user with 'enrol/course_tokens:unenrol' may unenrol others freely, false means nobody may touch user_enrolments.
      */
-    public function allow_unenrol($instance) {
+    public function allow_unenrol($instance)
+    {
         return false;
     }
 
@@ -70,7 +74,8 @@ class enrol_course_tokens_plugin extends enrol_plugin {
      * @since Moodle 3.1.
      * @return bool.
      */
-    public function use_standard_editing_ui() {
+    public function use_standard_editing_ui()
+    {
         return true;
     }
 
@@ -83,7 +88,8 @@ class enrol_course_tokens_plugin extends enrol_plugin {
      * @param context $context.
      * @return void
      */
-    public function edit_instance_form($instance, MoodleQuickForm $mform, $context) {
+    public function edit_instance_form($instance, MoodleQuickForm $mform, $context)
+    {
         // Do nothing by default.
     }
 
@@ -97,7 +103,8 @@ class enrol_course_tokens_plugin extends enrol_plugin {
      * @param context $context The context of the instance we are editing.
      * @return array Array of "element_name"=>"error_description" if there are errors, empty otherwise.
      */
-    public function edit_instance_validation($data, $files, $instance, $context) {
+    public function edit_instance_validation($data, $files, $instance, $context)
+    {
         // No errors by default.
         debugging('enrol_plugin::edit_instance_validation() is missing. This plugin has no validation!', DEBUG_DEVELOPER);
         return array();
@@ -110,43 +117,82 @@ class enrol_course_tokens_plugin extends enrol_plugin {
      * @param int $courseid.
      * @return bool.
      */
-    public function can_add_instance($courseid) {
+    public function can_add_instance($courseid)
+    {
         return true;
     }
-/**
- * Add new instance of enrol plugin.
- *
- * @param object $courseid The course object (stdClass).
- * @param array|null $fields Instance fields (optional)
- * @return int|bool The new instance id or false on error
- */
-public function add_instance($courseid, $fields = null) {
-    global $DB;
 
-    // Ensure courseid is an object and contains the 'id' property
-    if (!isset($courseid->id)) {
-        return false;
+    /**
+     * Is it possible to delete enrol instance via standard UI?
+     *
+     * @param stdClass $instance
+     * @return bool
+     */
+    public function can_delete_instance($instance)
+    {
+        $context = context_course::instance($instance->courseid);
+        return has_capability('enrol/course_tokens:config', $context);
     }
 
-    // Extract course id from the object
-    $courseid = (int)$courseid->id;
+    /**
+     * Add new instance of enrol plugin.
+     *
+     * @param object $courseid The course object (stdClass).
+     * @param array|null $fields Instance fields (optional)
+     * @return int|bool The new instance id or false on error
+     */
+    public function add_instance($courseid, $fields = null)
+    {
+        global $DB;
 
-    // Prepare minimal instance data.
-    $instance = [
-        'courseid' => $courseid,
-        'enrol' => 'course_tokens', // Correct plugin name
-        'status' => isset($fields['status']) ? (int)$fields['status'] : ENROL_INSTANCE_ENABLED, // Default to enabled
-        'timecreated' => time(),
-        'timemodified' => time(),
-    ];
+        // Ensure courseid is an object and contains the 'id' property
+        if (!isset($courseid->id)) {
+            return false;
+        }
 
-    // Insert the instance into the enrol table.
-    try {
-        $inserted = $DB->insert_record('enrol', $instance);
+        // Extract course id from the object
+        $courseid = (int) $courseid->id;
 
-        return $inserted; // Return the instance ID on success or false on failure
-    } catch (Exception $e) {
-        return false;
+        // Prepare minimal instance data.
+        $instance = [
+            'courseid' => $courseid,
+            'enrol' => 'course_tokens', // Correct plugin name
+            'status' => isset($fields['status']) ? (int) $fields['status'] : ENROL_INSTANCE_ENABLED, // Default to enabled
+            'timecreated' => time(),
+            'timemodified' => time(),
+        ];
+
+        // Insert the instance into the enrol table.
+        try {
+            $inserted = $DB->insert_record('enrol', $instance);
+
+            return $inserted; // Return the instance ID on success or false on failure
+        } catch (Exception $e) {
+            return false;
+        }
     }
-}
+
+    /**
+     * Deletes the enrol instance.
+     *
+     * @param object $instance The instance object.
+     * @return bool True on success, false on failure.
+     */
+    public function delete_instance($instance)
+    {
+        global $DB;
+
+        // Check if the instance exists
+        if (!$instance) {
+            return false;
+        }
+
+        // Delete the instance from the 'enrol' table
+        try {
+            $DB->delete_records('enrol', ['id' => $instance->id]);
+            return true; // Return true on success
+        } catch (Exception $e) {
+            return false; // Return false on failure
+        }
+    }
 }
