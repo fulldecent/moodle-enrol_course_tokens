58ad756a935ed8c25f8043dc9b06fe44464bc3e3
Raza403 <raza403@gmail.com>
2025-02-10 21:34:34 +0500

    Updated version.

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'enrol_course_tokens';
-$plugin->version   = 2024120304;  // Updated to the new version YYYYMMDDXX
+$plugin->version   = 2024120306;  // Updated to match the latest upgrade step
 $plugin->requires  = 2020061500;  // Requires this Moodle version
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.3 (Build: 2024120304)';  // Updated to match the new version
+$plugin->release   = '1.3 (Build: 2024120306)';  // Updated to match the new version
