dd1a7e981462a6766015dd03eec0b82962cc9504
Raza403 <raza403@gmail.com>
2026-05-02 17:03:01 +0500

    course tokens added pagination to index page. This PR is made for #431

--- version.php
+++ version.php
@@ -4,5 +4,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'enrol_course_tokens';
 $plugin->requires  = 2020061500;  // Requires this Moodle version
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->version   = 2026042902;
-$plugin->release   = '2.0.0';
+$plugin->version   = 2026050201;
+$plugin->release   = '2.1.0';
